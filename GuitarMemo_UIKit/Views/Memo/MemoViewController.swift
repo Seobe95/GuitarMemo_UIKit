b7b05e34224b5c360a6a48f88e7d12dca6bd6fbd
Seobe95 <woodyms@nate.com>
2024-06-26 00:53:08 +0900

    기본세팅 및 기본 화면 추가

--- GuitarMemo_UIKit/Views/Memo/MemoViewController.swift
+++ GuitarMemo_UIKit/Views/Memo/MemoViewController.swift
@@ -8,14 +8,27 @@
 import UIKit
 
 class MemoViewController: UIViewController {
+    private lazy var postingButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "Post",
+                                     style: .plain,
+                                     target: self,
+                                     action: #selector(postButtonTapped))
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.rightBarButtonItem = self.postingButton
     }
     
     func setTabBarItem() {
-        self.view.backgroundColor = .white
-        self.title = "Memo"
         self.tabBarItem = UITabBarItem(title: "Memo", image: UIImage(systemName: "note.text"), tag: 0)
+    }
+    
+    @objc
+    private func postButtonTapped() {
+        let postViewController = PostViewController()
+        postViewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(postViewController, animated: true)
     }
 }
